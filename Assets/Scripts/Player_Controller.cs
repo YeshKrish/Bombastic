146837a2c7629ada50ecf553c703d4031927e638
YeshKrish <yeshwinkrishna@gmail.com>
2022-12-24 12:48:25 +0530

    Blend Tree

--- Assets/Scripts/Player_Controller.cs
+++ Assets/Scripts/Player_Controller.cs
@@ -4,15 +4,40 @@ using UnityEngine;
 
 public class Player_Controller : MonoBehaviour
 {
+    Animator animator;
+
+    float _velocity = 0.0f;
+
+    public float acceleration = 0.1f;
+    public float deceleration = 0.5f;
+
+    int VelocityHash;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        animator = GetComponent<Animator>();
+        VelocityHash = Animator.StringToHash("Velocity");
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        bool forwardPressed = Input.GetKey("w");
+        bool runPressed = Input.GetKey("left shift");
+
+        if (forwardPressed && _velocity < 1.0f)
+        {
+            _velocity += Time.deltaTime * acceleration;
+        }
+        if (!forwardPressed && _velocity > 0.0f)
+        {
+            _velocity -= Time.deltaTime * deceleration;
+        }
+        if(!forwardPressed && _velocity < 0.0f)
+        {
+            _velocity = 0.0f;
+        }
+        animator.SetFloat(VelocityHash, _velocity);
     }
 }
